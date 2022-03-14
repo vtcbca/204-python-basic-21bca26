a7bf960b5d642cd9887b0b0ef56c22e22331d65f
21bca26 <96609077+21bca26@users.noreply.github.com>
2022-03-14 09:33:59 +0530

    Delete solution directory

--- solution/solution1.py
+++
@@ -1,5 +0,0 @@
-#WAS to enter 2 no and print sum of it
-a=int(input("Enter first number:"))
-b=int(input("Enter second number:"))
-c=a+b
-print("sum of {} and {} is : {}".format(a,b,c))
--- solution/solution2.py
+++
@@ -1,5 +0,0 @@
-#was to enter kilometer and converted it into meter
-i=int(input("Enter kilometer:"))
-a=0
-a=(i*1000)
-print(a,"meter")
